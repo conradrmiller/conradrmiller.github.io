622484087b5687fe9589a8c8bf219e4f69852178
Conrad Miller <42632539+conradrmiller@users.noreply.github.com>
2024-05-16 17:59:55 -0400

    added redirect to /resume route

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,12 +1,10 @@
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 const Resume = () => {
-    const navigate = useNavigate();
-    useEffect(() => {
-        navigate('/hosted-assets/Conrad-Miller-Resume.pdf', { replace: true });
-    });
-    return <div>Please wait to be redirected</div>;
+    return (
+        <meta
+            http-equiv="Refresh"
+            content="0; url='https://www.conradrmiller.com/hosted-assets/Conrad-Miller-Resume.pdf'"
+        />
+    );
 };
 
 export default Resume;
